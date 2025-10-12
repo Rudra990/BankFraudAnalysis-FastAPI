e2310f312b1c7971b339ad38b0dc34300523ac7f
rudra990 <rudraashutosh777@gmail.com>
2025-10-13 00:06:05 +0530

    local enviornment variables setup for main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,24 @@
+
+import os, sys
+print("CWD:", os.getcwd())
+print("__name__:", __name__)
+print("__package__:", __package__)
+print("sys.path[0]:", sys.path[0])
+
 from fastapi import FastAPI
 
+from core.config import settings
+from api.main import api_router
 
-app = FastAPI()
+
+app = FastAPI(
+  title = settings.PROJECT_NAME,
+  description= settings.PROJECT_DESCRIPTION,
+  docs_url = f"{settings.API_V1_STR}/docs",
+  redoc_url = f"{settings.API_V1_STR}/redoc",
+  openapi_url=f"{settings.API_V1_STR}/openapi.json",
 
 
-@app.get("/")
-def test():
-  return {"message":"test-ok"}
+)
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
