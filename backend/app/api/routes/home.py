f8e5447e5f50a91b2f719f457ad1b87c7e528073
rudra990 <rudraashutosh777@gmail.com>
2025-10-13 22:11:43 +0530

    added logger object in routes/home.py

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(prefix="/home")
 
 @router.get("/")
 def home():
+  logger.info("Home page accessed")  ##for testing logging
+  logger.debug("Home page accessed")
+  logger.error("Home page accessed")
   return {"message":"WELCOME TO HOMEPAGE!"}
