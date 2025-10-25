51a04df04ed7ea45a507a3720732014640e55be9
rudra990 <rudraashutosh777@gmail.com>
2025-10-25 15:32:54 +0530

    added comments for better understanding

--- backend/docker/local/fastapi/start.sh
+++ backend/docker/local/fastapi/start.sh
@@ -3,9 +3,9 @@
 
 set -o errexit 
 
-set -o nounset 
+set -o nounset   #helps to catch bugs related to undefined variables
 
-set -o pipefail
+set -o pipefail  #script exits with a non zero status if any pipeline fails 
 
-exec uvicorn backend.app.main:app --host 0.0.0.0 --port 8000 --reload 
+exec uvicorn backend.app.main:app --host 0.0.0.0 --port 8000 --reload #fastapi backend location
 
